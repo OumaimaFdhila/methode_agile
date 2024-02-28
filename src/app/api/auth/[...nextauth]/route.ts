d1db0edd9fb30298c21bf048613b56aa44243f5e
AmineGuitouni <Notam1ne2002@gmail.com>
2024-02-28 20:38:14 +0100

    auth fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-    // ...add more providers here
-  ],
-}
+import { authOptions } from "@/lib/authOption"
 
 const handler = NextAuth(authOptions)
 
